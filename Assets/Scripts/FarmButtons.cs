59bba8c1bfb9c813ed0c2b6df6cf2e3d917bd8ab
AlysonMoraiis <alysonmorais35@hotmail.com>
2021-09-30 10:01:18 -0300

    Correção 1
    Arrumei alguns bugs

---
+++ Assets/Scripts/FarmButtons.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FarmButtons : MonoBehaviour
+{
+    private int carrotLvl = 1;
+
+    [SerializeField]
+    private Text textCarrotLvl;
+    [SerializeField]
+    private CoinsData coinsData;
+    
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void Carrot()
+    {
+        carrotLvl += 1;
+        textCarrotLvl.text = carrotLvl.ToString();
+    }
+}
